4b748ea9b1c581d5a532d58f27b8aa48509a37ca
Danielle Okezie <danielleokezie@college.harvard.edu>
2017-11-26 01:15:08 -0500

    still working on quiz results

--- js/vis-bar.js
+++ js/vis-bar.js
@@ -1,32 +1,3 @@
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 1060 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-// set the ranges
-var x = d3.scaleBand()
-    .range([0, width])
-    .padding(0.1);
-var y = d3.scaleLinear()
-    .range([height, 0]);
-
-
-
-// append the svg object to the body of the page
-// append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("#barChart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-
-var svg2 = d3.select("#mapChart").append("svg")
-    .attr("width", 960)
-    .attr("height", 500);
-
 var stateIDs = {
     "Alabama": 1, "Alaska": 2, "Arizona": 4, "Arkansas": 5, "California": 6, "Colorado": 8,
     "Connecticut": 9, "Delaware": 10, "DistrictofColumbia": 11, "Florida": 12, "Georgia": 13,
@@ -40,10 +11,19 @@ var stateIDs = {
     "West Virginia": 54, "Wisconsin": 55, "Wyoming": 56
 };
 
-var selectMe = "Alabama";
+var selectMe = "Texas";
 
+d3.select("#africaD").on("change", function() {
+
+    selectMe = d3.select("#africaD").property("value");
+
+    //updateChoropleth();
+
+});
 var data = {};
 
+var width = 700,
+    height = 400;
 
 var projection = d3.geoAlbers();
 
@@ -51,10 +31,13 @@ var projection = d3.geoAlbers();
 var path = d3.geoPath()
     .projection(projection);
 
-var tooltips = d3.select("#mapchart").append("div")
-    .attr("class", "tooltips")
-    .style("opacity", 0);
+var svg = d3.select("#mapchart").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
+var tooltip = d3.select("#mapchart").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
 queue()
     .defer(d3.json, "data/us.json")
@@ -78,28 +61,28 @@ queue()
 
         var b = path.bounds(state),
             s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
-            t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+            t = [350, (height - s * (b[1][1] + b[0][1])) / 2];
 
         projection.scale(s)
             .translate(t);
 
-        svg2.append("path")
+        svg.append("path")
             .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
             .attr("class", "mesh")
             .attr("d", path);
 
-        svg2.append("path")
+        svg.append("path")
             .datum(state)
             .attr("class", "outline")
             .attr("d", path)
             .attr('id', 'land');
 
-        svg2.append("clipPath")
+        svg.append("clipPath")
             .attr("id", "clip-land")
             .append("use")
             .attr("xlink:href", "#land");
 
-        svg2.selectAll("path")
+        svg.selectAll("path")
             .data(topojson.feature(us, us.objects.counties).features)
             .enter().append("path")
             .attr("d", path)
@@ -107,73 +90,76 @@ queue()
                 return d.id;
             }).attr("clip-path", "url(#clip-land)")
             .attr('class', 'county')
-            .on("mouseover", function(d, i) {
-                tooltips.transition()
+            .on("mousemove", function(d, i) {
+                tooltip.transition()
                     .duration(200)
                     .style("opacity", .9);
-                tooltips.html(data[d.id][0] + "<br>" + data[d.id][1])
-                    .style("left", (d3.event.pageX) + "px")
-                    .style("top", (d3.event.pageY - 28) + "px");
+                tooltip.html(data[d.id][0] + "<br>" + data[d.id][1] + "%")
+                    .style("left", (d3.event.pageX - 100) + "px")
+                    .style("top", (d3.event.pageY - 200) + "px");
             })
             .on("mouseout", function(d) {
-                tooltips.transition()
+                tooltip.transition()
                     .duration(500)
                     .style("opacity", 0);
             });
 
     });
+var margin = {top: 50, right: 50, bottom: 50, left: 50}
+    , widthh = 500 - margin.left - margin.right // Use the window's width
+    , heightt = 500 - margin.top - margin.bottom; // Use the window's height
 
+// The number of datapoints
+var n = 21;
 
-var tooltip = d3.select("#barChart").append("div").attr("class", "toolTip");
+// 5. X scale will use the index of our data
+var xScale = d3.scaleLinear()
+    .domain([0, n-1]) // input
+    .range([0, widthh]); // output
 
-// get the data
-d3.csv("data/nclimate2583-s2.csv", function(error, data) {
-    if (error) throw error;
+// 6. Y scale will use the randomly generate number
+var yScale = d3.scaleLinear()
+    .domain([0, 1]) // input
+    .range([heightt, 0]); // output
 
-    // format the data
-    data.forEach(function(d) {
-        d.sales = +d.sales;
-    });
+// 7. d3's line generator
+var line = d3.line()
+    .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
+    .y(function(d) { return yScale(d.y); }) // set the y values for the line generator
+    .curve(d3.curveMonotoneX) // apply smoothing to the line
 
-    // Scale the range of the data in the domains
-    x.domain(data.map(function(d) { return d.State_abb; }));
-    y.domain([0, 100]);
+// 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
+var dataset = d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
 
-    // append the rectangles for the bar chart
-    svg.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.State_abb); })
-        .attr("width", x.bandwidth())
-        .attr("y", function(d) { return y(d.x65_happening); })
-        .attr("height", function(d) { return height - y(d.x65_happening); })
-        .on("mousemove", function(d){
-            tooltip
-                .style("left", d3.event.pageX - 80 + "px")
-                .style("top", d3.event.pageY - 100 + "px")
-                .style("display", "inline-block")
-                .html(String(d.x65_happening) + "%");
-        })
-        .on("mouseout", function(d){ tooltip.style("display", "none");});
+// 1. Add the SVG to the page and employ #2
+var svg2 = d3.select("#linechart").append("svg")
+    .attr("width", widthh + margin.left + margin.right)
+    .attr("height", heightt + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // add the x Axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+// 3. Call the x axis in a group tag
+svg2.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + heightt + ")")
+    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
 
-    // add the y Axis
-    svg.append("g")
-        .call(d3.axisLeft(y));
+// 4. Call the y axis in a group tag
+svg2.append("g")
+    .attr("class", "y axis")
+    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(y)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Percentage(%)");
+// 9. Append the path, bind the data, and call the line generator
+svg2.append("path")
+    .datum(dataset) // 10. Binds data to the line
+    .attr("class", "line") // Assign a class for styling
+    .attr("d", line); // 11. Calls the line generator
 
-});
+// 12. Appends a circle for each datapoint
+svg2.selectAll(".dot")
+    .data(dataset)
+    .enter().append("circle") // Uses the enter().append() method
+    .attr("class", "dot") // Assign a class for styling
+    .attr("cx", function(d, i) { return xScale(i) })
+    .attr("cy", function(d) { return yScale(d.y) })
+    .attr("r", 5);
